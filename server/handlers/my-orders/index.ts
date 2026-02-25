c9c061f05389a9ca16500f65af854f6972138bce
Jonas L <jl.7804@gmail.com>
2026-02-25 19:23:54 +0100

    test

--- server/handlers/my-orders/index.ts
+++ server/handlers/my-orders/index.ts
@@ -1,4 +1,4 @@
-import { ensureSchemaAndSeed, listOrders } from '../lib/_shop.js'
+import { ensureSchemaAndSeed, listOrders } from '../../../api/lib/_shop.js'
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'GET') {
